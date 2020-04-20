c70b031495c998129ee43b2d91f144871d9f096f
François Rozet <francois.rozet@outlook.com>
2020-04-21 01:06:55 +0200

    Writing Web Map Tile Service package

--- python/walonmap.py
+++ python/walonmap.py
@@ -7,83 +7,20 @@
 
 import cv2
 import numpy as np
-import pyproj as pp
 
-from owslib.wmts import WebMapTileService
-from PIL import Image
+from wmts import WMTS
 
 
 #############
 # Constants #
 #############
 
-_WMTS = WebMapTileService('https://geoservices.wallonie.be/arcgis/rest/services/IMAGERIE/ORTHO_LAST/MapServer/WMTS')
-_LAYER = 'IMAGERIE_ORTHO_LAST'
-_TILE_MATRIX_SET = 'default028mm'
-_TILE_MATRIX = '15'
-_FORMAT = 'image/jpg'
-
-_WIDTH = 512
-_HEIGHT = 512
-
-_WGS84 = 'EPSG:4326' # World Geodetic System 84
-_LAMBERT = 'EPSG:31370' # Belgian Lambert 72
-
-_TO_LAMBERT = pp.Transformer.from_crs(_WGS84, _LAMBERT)
-_TO_WGS84 = pp.Transformer.from_crs(_LAMBERT, _WGS84)
-
-_X_MIN = 42000
-_X_MAX = 296000
-_Y_MIN = 20000
-_Y_MAX = 168000
-
-_ROW_MIN = 609074
-_ROW_MAX = 611259
-_COL_MIN = 530235
-_COL_MAX = 533985
-
-_TILE_WIDTH = (_X_MAX - _X_MIN) / (_COL_MAX - _COL_MIN + 1)
-_TILE_HEIGHT = (_Y_MAX - _Y_MIN) / (_ROW_MAX - _ROW_MIN + 1)
-
-
-#############
-# Functions #
-#############
-
-def xy_to_wgs(x, y):
-	return _TO_WGS84.transform(x, y)
-
-
-def wgs_to_xy(lat, lon):
-	return _TO_LAMBERT.transform(lat, lon)
-
-
-def tile_to_xy(row, col):
-	x = _X_MIN + (col - _COL_MIN) * _TILE_WIDTH
-	y = _Y_MAX - (row - _ROW_MIN) * _TILE_HEIGHT
-
-	return x, y
-
-
-def xy_to_tile(x, y, integer=True):
-	col = _COL_MIN + (x - _X_MIN) / _TILE_WIDTH
-	row = _ROW_MIN + (_Y_MAX - y) / _TILE_HEIGHT
-
-	if integer:
-		row, col = int(row), int(col)
-
-	return row, col
-
-
-def get_tile(row, col):
-	return Image.open(_WMTS.gettile(
-		layer=_LAYER,
-		tilematrixset=_TILE_MATRIX_SET,
-		tilematrix=_TILE_MATRIX,
-		row=row,
-		column=col,
-		format=_FORMAT
-	))
+_WALONMAP = WMTS(
+	wmts='https://geoservices.wallonie.be/arcgis/rest/services/IMAGERIE/ORTHO_LAST/MapServer/WMTS',
+	layer='IMAGERIE_ORTHO_LAST',
+	tms='default028mm',
+	tm='15'
+)
 
 
 ###########
@@ -131,6 +68,8 @@ if __name__ == '__main__':
 
 	import via as VIA
 
+	from PIL import Image
+
 	from models import UNet
 	from dataset import to_pil, to_tensor, to_polygons
 
@@ -141,25 +80,17 @@ if __name__ == '__main__':
 	parser.add_argument('-m', '--model', default='../products/models/unet.pth', help='model')
 	parser.add_argument('-n', '--name', default='', help='prefix name of the tiles')
 	parser.add_argument('-o', '--output', default='../products/json/walonmap.json', help='output VIA file')
-	parser.add_argument('-p', '--polygon', default=None, help='GeoJSON polygon file')
+	parser.add_argument('-p', '--polygon', required=True, help='GeoJSON polygon file')
 	args = parser.parse_args()
 
 	# Contour
-	if args.polygon is None:
-		contour = [
-			(_ROW_MIN, _COL_MIN),
-			(_ROW_MIN, _COL_MAX),
-			(_ROW_MAX, _COL_MAX),
-			(_ROW_MAX, _COL_MIN)
-		]
-	else:
-		with open(args.polygon, 'r') as f:
-			geojson = json.load(f)
+	with open(args.polygon, 'r') as f:
+		geojson = json.load(f)
 
-		contour = list(map(
-			lambda x: xy_to_tile(*wgs_to_xy(*reversed(x))),
-			geojson['coordinates'][0]
-		))
+	contour = list(map(
+		lambda x: _WALONMAP.wgs_to_tile(x[1], x[0]),
+		geojson['coordinates'][0]
+	))
 
 	contour = Contour(contour)
 
@@ -189,7 +120,7 @@ if __name__ == '__main__':
 				args.limit -= 1
 
 			try:
-				tile = get_tile(row, col)
+				tile = Image.open(_WALONMAP.get_tile(row, col))
 			except Exception:
 				continue
 
---
+++ python/wmts.py
@@ -0,0 +1,120 @@
+"""
+Web Map Tile Service package
+----------
+OpenGIS Web Map Tile Service Implementation Standard
+(Maso, Pomakis and Julia, 2010)
+https://www.ogc.org/standards/wmts
+"""
+
+#############
+# Libraries #
+#############
+
+import pyproj as pp
+
+from owslib.wmts import WebMapTileService
+
+
+#############
+# Constants #
+#############
+
+_PIXEL_SIZE = 0.28e-3
+_WGS84 = 'EPSG:4326' # World Geodetic System 84
+
+
+###########
+# Classes #
+###########
+
+class WMTS:
+	'''Web Map Tile Service wrapper'''
+
+	def __init__(self, wmts, layer=None, tms=None, tm=None, fmt=None, pixel_size=_PIXEL_SIZE):
+		# Web Map Tile Service
+		self.wmts = WebMapTileService(wmts)
+
+		if layer is None:
+			layer = next(iter(self.wmts.contents))
+		self.layer = self.wmts.contents[layer]
+
+		if tms is None:
+			tms = next(iter(self.wmts.tilematrixsets))
+		self.tms = self.wmts.tilematrixsets[tms]
+
+		if tm is None:
+			tm = next(iter(self.tms.tilematrix))
+		self.tm = self.tms.tilematrix[tm]
+
+		# CRS
+		self.crs = pp.CRS.from_user_input(self.tms.crs)
+		self.epsg = ':'.join(self.crs.to_authority())
+
+		self._from_wgs = pp.Transformer.from_crs(_WGS84, self.crs)
+		self._to_wgs = pp.Transformer.from_crs(self.crs, _WGS84)
+
+		self.unit = self.crs.coordinate_system.axis_list[0].unit_name
+		self.ucf = self.crs.coordinate_system.axis_list[0].unit_conversion_factor
+
+		# Tile span(s)
+		self.pixel_size = pixel_size
+		self.scale = self.tm.scaledenominator
+		self.pixel_span = self.scale * self.pixel_size * self.ucf
+
+		self.tile_width = self.tm.tilewidth
+		self.tile_height = self.tm.tileheight
+
+		self.tile_span_x = self.tile_width * self.pixel_span
+		self.tile_span_y = self.tile_height * self.pixel_span
+
+		# Domain
+		self.matrix_width = self.tm.matrixwidth
+		self.matrix_height = self.tm.matrixheight
+
+		self.x_min, self.y_max = self.tm.topleftcorner
+		self.x_max = self.x_min + self.matrix_width * self.tile_span_x
+		self.y_min = self.y_max - self.matrix_height * self.tile_span_y
+
+		# Format
+		self.fmt = self.layer.formats[0] if fmt is None else fmt
+
+	def get_tile(self, row, col):
+		return self.wmts.gettile(
+			layer=self.layer.id,
+			tilematrixset=self.tms.identifier,
+			tilematrix=self.tm.identifier,
+			row=row,
+			column=col,
+			format=self.fmt
+		)
+
+	def tile_to_xy(self, row, col):
+		x = self.x_min + col * self.tile_span_x
+		y = self.y_max - row * self.tile_span_y
+
+		return x, y
+
+	def xy_to_tile(self, x, y, integer=True):
+		col = (x - self.x_min) / self.tile_span_x
+		row = (self.y_max - y) / self.tile_span_y
+
+		if integer:
+			row, col = int(row), int(col)
+
+		return row, col
+
+	def xy_to_wgs(self, x, y):
+		return self._to_wgs.transform(x, y)
+
+	def wgs_to_xy(self, lat, lon):
+		return self._from_wgs.transform(lat, lon)
+
+	def tile_to_wgs(self, row, col):
+		return self.xy_to_wgs(*self.tile_to_xy(row, col))
+
+	def wgs_to_tile(self, lat, lon):
+		return self.xy_to_tile(*self.wgs_to_xy(lat, lon))
+
+	@property
+	def domain(self):
+		return self.x_min, self.y_min, self.x_max, self.y_max
