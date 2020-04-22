a2ad3301841536c68e6d1ebdd591ade6779541e7
François Rozet <francois.rozet@outlook.com>
2020-04-22 13:32:21 +0200

    Adding GitHub page

---
+++ docs/resources/js/colors.js
@@ -0,0 +1,12 @@
+var colors = {
+	blue: '#03A9F4',
+	teal: '#009688',
+	green: '#4CAF50',
+	lime: '#CDDC39',
+	yellow: '#FFEB3B',
+	amber: '#FFC107',
+	orange: '#FF9800',
+	red: '#F44336',
+	pink: '#E91E63',
+	purple: '#9C27B0'
+}
